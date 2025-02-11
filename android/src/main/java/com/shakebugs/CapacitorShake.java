33d74c9c5baad641c30f17d082edf8b43a0d0477
alexautograb <alex.nguyen@autograb.com.au>
2025-02-11 16:36:07 +1100

    fix: run on main thread

--- android/src/main/java/com/shakebugs/CapacitorShake.java
+++ android/src/main/java/com/shakebugs/CapacitorShake.java
@@ -7,7 +7,12 @@ import com.shakebugs.shake.Shake;
 public class CapacitorShake {
     public void start(Activity activity, String apiKey) {
         Shake.setCrashReportingEnabled(true);
-        Shake.start(activity, apiKey);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Shake.start(activity, apiKey);
+            }
+        });
         return;
     }
 
